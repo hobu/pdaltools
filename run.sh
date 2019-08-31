2c6ba626b3b21f9c320c9a8c05cdec703e9b339a
Howard Butler <howard@hobu.co>
2019-08-31 18:40:17 -0500

    cleanup

--- run.sh
+++
@@ -1,8 +0,0 @@
-source enviro.sh
-
-#ls laz/*.laz | parallel $DRY $RATE ./crop.sh {}
-#echo "cropped/5142-11-30.laz" |  parallel $DRY $RATE ./ground.sh {}
-#echo "cropped/5142-11-30.laz" |  parallel $DRY $RATE ./dtm.sh {}
-ls original/*.las | parallel $DRY $RATE ./compress.sh {}
-#ls cropped/*.laz | parallel $DRY $RATE ./ground.sh {}
-#./raster.sh
